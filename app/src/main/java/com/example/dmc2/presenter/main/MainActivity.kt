f047172a7fef053996dca6c7b03a4d75cd713ba1
Bandal <no1msh1217@gmail.com>
2023-08-14 23:42:49 +0900

    Initial commit

---
+++ app/src/main/java/com/example/dmc2/presenter/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dmc2.presenter.main
+
+import android.os.Bundle
+import com.example.dmc2.R
+import com.example.dmc2.common.BindingActivity
+import com.example.dmc2.databinding.ActivityMainBinding
+
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
