7a9ae4079ae0940b39895f1c77b2748fb4469c40
Bandal <no1msh1217@gmail.com>
2023-08-28 22:55:34 +0900

    feat: 음식추천값 받아오는 서버통신 로직 Repository 패턴 적용

---
+++ app/src/main/java/com/monthlycoding/dmc2/data/remote/response/FoodRecommendDto.kt
@@ -0,0 +1,26 @@
+package com.monthlycoding.dmc2.data.remote.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class FoodRecommendDto(
+    @SerialName("category_id")
+    val categoryId: Int,
+    @SerialName("address")
+    val address: String,
+    @SerialName("closed_days")
+    val closedDays: String,
+    @SerialName("latitude")
+    val latitude: String,
+    @SerialName("longitude")
+    val longitude: String,
+    @SerialName("naver_link")
+    val naverLink: String,
+    @SerialName("operation_hours")
+    val operationHours: String,
+    @SerialName("store_id")
+    val storeId: Int,
+    @SerialName("store_name")
+    val storeName: String
+)
