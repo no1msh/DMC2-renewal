a86e2a828f42eaed0f8301c734089f720705b72e
Bandal <no1msh1217@gmail.com>
2023-09-03 16:17:42 +0900

    feat: 복불복 구현

--- app/src/main/java/com/monthlycoding/dmc2/presenter/hitandmiss/HitAndMissViewModel.kt
+++ app/src/main/java/com/monthlycoding/dmc2/presenter/hitandmiss/HitAndMissViewModel.kt
@@ -1,7 +1,14 @@
 package com.monthlycoding.dmc2.presenter.hitandmiss
 
 import androidx.lifecycle.ViewModel
+import com.monthlycoding.domain.hitandmiss.HitAndMissGame
 
 class HitAndMissViewModel: ViewModel() {
+
+    lateinit var hitAndMissGame: HitAndMissGame
+
+    fun initHitAndMissGame(hitCount: Int){
+        hitAndMissGame = HitAndMissGame(hitCount)
+    }
 
 }
