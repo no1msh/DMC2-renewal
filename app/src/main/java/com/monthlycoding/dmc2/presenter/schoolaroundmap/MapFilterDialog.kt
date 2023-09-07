4a4a3daec953e2fd5d4ed939151ed1638ac6bd4b
Bandal <no1msh1217@gmail.com>
2023-09-08 01:32:14 +0900

    feat: 지도에서 사용할 filter 기능 구현

---
+++ app/src/main/java/com/monthlycoding/dmc2/presenter/schoolaroundmap/MapFilterDialog.kt
@@ -0,0 +1,104 @@
+package com.monthlycoding.dmc2.presenter.schoolaroundmap
+
+import android.app.Dialog
+import android.content.Context
+import android.util.TypedValue
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.children
+import androidx.core.view.setMargins
+import androidx.databinding.DataBindingUtil
+import com.monthlycoding.dmc2.R
+import com.monthlycoding.dmc2.custom.CuisineView
+import com.monthlycoding.dmc2.databinding.DialogMapFilterBinding
+import com.monthlycoding.dmc2.presenter.foodrecommend.model.Cuisine
+
+class MapFilterDialog(
+    context: Context,
+    categorySelectDone: MapFilterDialogClickListener
+) : Dialog(context) {
+
+    private val binding: DialogMapFilterBinding by lazy {
+        DataBindingUtil
+            .inflate(LayoutInflater.from(context), R.layout.dialog_map_filter, null, false)
+    }
+
+    init {
+        setContentView(binding.root)
+        initCuisineViews()
+        addTotalCuisineView()
+
+        binding.tvMapFilterButtonDone.setOnClickListener {
+            dismiss()
+            val temp: List<CuisineView> =
+                binding.glMapFilterCuisineList.children.map { it as CuisineView }.toList()
+            val ids: List<Int> = temp
+                .filter { it.isSelected }
+                .map { it.categoryId }
+                .filter { it != CuisineView.TOTAL_CATEGORY_ID }
+            categorySelectDone.onCategorySelectDoneClick(ids)
+        }
+
+    }
+
+    private fun initCuisineViews() {
+        Cuisine.values().forEach {
+            val cuisineView = createStandardCuisineView()
+            cuisineView.setTitle(it.title)
+            cuisineView.setIcon(it.icon)
+            cuisineView.setCategoryId(it.id)
+            binding.glMapFilterCuisineList.addView(cuisineView)
+        }
+    }
+
+    private fun addTotalCuisineView() {
+        val totalCuisine = createStandardCuisineView()
+        totalCuisine.setTitle(context.getString(R.string.food_recommend_total_cuisine_view_title))
+        totalCuisine.setOnClickListener {
+            totalCuisine.isSelected = !totalCuisine.isSelected
+            binding.glMapFilterCuisineList.children
+                .map { it as CuisineView }
+                .forEach { cuisineView ->
+                    cuisineView.isSelected = totalCuisine.isSelected
+                    changeCuisineViewTitleTextColor(cuisineView)
+                }
+        }
+        binding.glMapFilterCuisineList.addView(totalCuisine)
+    }
+
+    private fun createStandardCuisineView(): CuisineView {
+        val cuisineView = CuisineView(context)
+        val layoutParams = ConstraintLayout.LayoutParams(
+            getDpToPixel(DEFAULT_CUISINE_VIEW_WIDTH),
+            getDpToPixel(DEFAULT_CUISINE_VIEW_HEIGHT),
+        ).apply { this.setMargins(getDpToPixel(DEFAULT_CUISINE_VIEW_MARGIN)) }
+
+        cuisineView.layoutParams = layoutParams
+
+        cuisineView.setOnClickListener {
+            it.isSelected = !it.isSelected
+            changeCuisineViewTitleTextColor(it as CuisineView)
+        }
+        return cuisineView
+    }
+
+    private fun changeCuisineViewTitleTextColor(cuisineView: CuisineView) {
+        if (cuisineView.isSelected) {
+            cuisineView.title.setTextColor(ContextCompat.getColor(context, R.color.white))
+            return
+        }
+        cuisineView.title.setTextColor(ContextCompat.getColor(context, R.color.black))
+    }
+
+    private fun getDpToPixel(dp: Int) = TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP,
+        dp.toFloat(), context.resources.displayMetrics
+    ).toInt()
+
+    companion object {
+        private const val DEFAULT_CUISINE_VIEW_WIDTH = 100
+        private const val DEFAULT_CUISINE_VIEW_HEIGHT = 100
+        private const val DEFAULT_CUISINE_VIEW_MARGIN = 10
+    }
+}
--- app/src/main/java/com/monthlycoding/dmc2/presenter/schoolaroundmap/SchoolAroundMapActivity.kt
+++ app/src/main/java/com/monthlycoding/dmc2/presenter/schoolaroundmap/SchoolAroundMapActivity.kt
@@ -3,17 +3,60 @@ package com.monthlycoding.dmc2.presenter.schoolaroundmap
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
 import com.monthlycoding.dmc2.R
 import com.monthlycoding.dmc2.common.BindingActivity
+import com.monthlycoding.dmc2.common.showDefaultToast
 import com.monthlycoding.dmc2.databinding.ActivitySchoolAroundMapBinding
 
 class SchoolAroundMapActivity :
-    BindingActivity<ActivitySchoolAroundMapBinding>(R.layout.activity_school_around_map) {
+    BindingActivity<ActivitySchoolAroundMapBinding>(R.layout.activity_school_around_map),
+    MapFilterDialogClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        initActionBar()
+    }
+
+    private fun initActionBar() {
+        setSupportActionBar(binding.tbSchoolAroundMapToolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeActionContentDescription(R.string.toolbar_back_text)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.filter_menu, menu)
+        menu?.findItem(R.id.filter)?.actionView?.let { view ->
+            view.setOnClickListener { showFilterDialog() }
+        }
+        return true
+    }
+
+    private fun showFilterDialog() {
+        MapFilterDialog(this, this).apply {
+            val density = resources.displayMetrics.density * 1.2
+            window?.setLayout(
+                (DEFAULT_DIALOG_WIDTH * density).toInt(),
+                (DEFAULT_DIALOG_HEIGHT * density).toInt()
+            )
+            show()
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
+    }
+
+    override fun onCategorySelectDoneClick(categoryIds: List<Int>) {
+        showDefaultToast(this, "카테고리 $categoryIds")
     }
 
     companion object {
+        private const val DEFAULT_DIALOG_WIDTH = 340
+        private const val DEFAULT_DIALOG_HEIGHT = 600
+
         fun getIntent(context: Context): Intent =
             Intent(context, SchoolAroundMapActivity::class.java)
     }
