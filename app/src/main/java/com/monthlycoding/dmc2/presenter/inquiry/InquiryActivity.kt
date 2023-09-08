67cf3ebf8518cea3b8892bb0a45415c4df29aaec
Bandal <no1msh1217@gmail.com>
2023-09-08 23:41:43 +0900

    feat: 문의하기 페이지 뷰 구현

--- app/src/main/java/com/monthlycoding/dmc2/presenter/inquiry/InquiryActivity.kt
+++ app/src/main/java/com/monthlycoding/dmc2/presenter/inquiry/InquiryActivity.kt
@@ -3,6 +3,7 @@ package com.monthlycoding.dmc2.presenter.inquiry
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.monthlycoding.dmc2.R
 import com.monthlycoding.dmc2.common.BindingActivity
 import com.monthlycoding.dmc2.databinding.ActivityInquiryBinding
@@ -10,6 +11,17 @@ import com.monthlycoding.dmc2.databinding.ActivityInquiryBinding
 class InquiryActivity : BindingActivity<ActivityInquiryBinding>(R.layout.activity_inquiry) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding.spinnerInquiryType.selectedItem
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.inquiry_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.spinnerInquiryType.adapter = adapter
+        }
     }
 
     companion object {
