f64250e7a5bbe2320e1536c68bce336c5d23e6de
Bandal <no1msh1217@gmail.com>
2023-09-30 22:47:07 +0900

    chore: 1.0.1으로 버전업

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.monthlycoding.dmc2"
         minSdk = 27
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "DMC2_BASE_URL", getApiKey("BASE_URL"))
